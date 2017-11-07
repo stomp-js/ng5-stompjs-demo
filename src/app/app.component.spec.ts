3594c458f98e7c4523b7d274c6dbf94e600f2c8c
deepak <deepak@kreatio.com>
2017-11-07 20:40:02 +0530

    Angular 5

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,30 @@
+/* tslint:disable:no-unused-variable */
+
 import { TestBed, async } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
-describe('AppComponent', () => {
-  beforeEach(async(() => {
+import { RawDataComponent } from './components/rawdata/rawdata.component';
+import { StatusComponent } from './components/status/status.component';
+
+describe('App: Ng2StompjsDemo', () => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        RawDataComponent,
+        StatusComponent
       ],
-    }).compileComponents();
-  }));
+      imports: [
+        HttpModule
+      ],
+      providers: []
+    });
+  });
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
---
+++ src/app/components/status/status.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { StompService, StompState } from '@stomp/ng2-stompjs';
+
+@Component({
+  selector: 'app-status',
+  templateUrl: './status.component.html',
+  styleUrls: ['./status.component.css']
+})
+export class StatusComponent implements OnInit {
+
+  public state: Observable<string>;
+
+  /** Constructor */
+  constructor(private _stompService: StompService) { }
+
+  ngOnInit() {
+    console.log('Status init');
+    this.state = this._stompService.state
+      .map((state: number) => StompState[state]);
+  }
+}
